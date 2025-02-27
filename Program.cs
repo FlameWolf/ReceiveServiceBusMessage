7e1eec344ba0bfe514ec97f51fef69855c05d978
FlameWolf <agnijith@gmail.com>
2025-02-27 18:18:28 +0530

    Refactor code and enable Application Insights

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
-
-builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var builder = FunctionsApplication.CreateBuilder(args);
+		builder.ConfigureFunctionsWebApplication();
+		builder.Services
+			.AddApplicationInsightsTelemetryWorkerService()
+			.ConfigureFunctionsApplicationInsights();
+		builder.Build().Run();
+	}
+}
